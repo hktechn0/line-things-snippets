ed1f83a429b8dc118a62bb37725824ef4c9711c2
Hirotaka Kawata <kawata.hirotaka@linecorp.com>
2019-04-16 19:36:37 +0900

    Add LIFF app for air quality monitor service

---
+++ liff-app/air-quality/liff-starter.js
@@ -0,0 +1,342 @@
+const ENVIRONMENTAL_SENSING_SERVICE_UUID = "0000181A-0000-1000-8000-00805F9B34FB";
+const PRESSURE_CHARACTERISTIC_UUID = "00002A6D-0000-1000-8000-00805F9B34FB";
+const TEMPERATURE_CHARACTERISTIC_UUID = "00002A6E-0000-1000-8000-00805F9B34FB";
+const HUMIDTY_CHARACTERISTIC_UUID = "00002A6F-0000-1000-8000-00805F9B34FB";
+const AIR_QUALITY_MONITOR_SERVICE_UUID = "288BDFE4-7F85-47A6-9908-3B8F29CE69E5"
+const CO2_CHARACTERISTIC_UUID = "9F3F3F1D-3763-4D1F-8031-58C728D7794B"
+const TVOC_CHARACTERISTIC_UUID = "00CF24AA-BC94-437E-ABD4-241D26F72779"
+
+
+const deviceUUIDSet = new Set();
+const connectedUUIDSet = new Set();
+const connectingUUIDSet = new Set();
+
+let logNumber = 1;
+
+function onScreenLog(text) {
+    const logbox = document.getElementById('logbox');
+    logbox.value += '#' + logNumber + '> ';
+    logbox.value += text;
+    logbox.value += '\n';
+    logbox.scrollTop = logbox.scrollHeight;
+    logNumber++;
+}
+
+window.onload = () => {
+    liff.init(async () => {
+        onScreenLog('LIFF initialized');
+        renderVersionField();
+
+        await liff.initPlugins(['bluetooth']);
+        onScreenLog('BLE plugin initialized');
+
+        checkAvailablityAndDo(() => {
+            onScreenLog('Finding devices...');
+            findDevice();
+        });
+    }, e => {
+        flashSDKError(e);
+        onScreenLog(`ERROR on getAvailability: ${e}`);
+    });
+}
+
+async function checkAvailablityAndDo(callbackIfAvailable) {
+    const isAvailable = await liff.bluetooth.getAvailability().catch(e => {
+        flashSDKError(e);
+        onScreenLog(`ERROR on getAvailability: ${e}`);
+        return false;
+    });
+    // onScreenLog("Check availablity: " + isAvailable);
+
+    if (isAvailable) {
+        document.getElementById('alert-liffble-notavailable').style.display = 'none';
+        callbackIfAvailable();
+    } else {
+        document.getElementById('alert-liffble-notavailable').style.display = 'block';
+        setTimeout(() => checkAvailablityAndDo(callbackIfAvailable), 1000);
+    }
+}
+
+// Find LINE Things device using requestDevice()
+async function findDevice() {
+    const device = await liff.bluetooth.requestDevice().catch(e => {
+        flashSDKError(e);
+        onScreenLog(`ERROR on requestDevice: ${e}`);
+        throw e;
+    });
+    // onScreenLog('detect: ' + device.id);
+
+    try {
+        if (!deviceUUIDSet.has(device.id)) {
+            deviceUUIDSet.add(device.id);
+            addDeviceToList(device);
+        } else {
+            // TODO: Maybe this is unofficial hack > device.rssi
+            document.querySelector(`#${device.id} .rssi`).innerText = device.rssi;
+        }
+
+        checkAvailablityAndDo(() => setTimeout(findDevice, 100));
+    } catch (e) {
+        onScreenLog(`ERROR on findDevice: ${e}\n${e.stack}`);
+    }
+}
+
+// Add device to found device list
+function addDeviceToList(device) {
+    onScreenLog('Device found: ' + device.name);
+
+    const deviceList = document.getElementById('device-list');
+    const deviceItem = document.getElementById('device-list-item').cloneNode(true);
+    deviceItem.setAttribute('id', device.id);
+    deviceItem.querySelector(".device-id").innerText = device.id;
+    deviceItem.querySelector(".device-name").innerText = device.name;
+    deviceItem.querySelector(".rssi").innerText = device.rssi;
+    deviceItem.classList.add("d-flex");
+    deviceItem.addEventListener('click', () => {
+        deviceItem.classList.add("active");
+        try {
+            connectDevice(device);
+        } catch (e) {
+            onScreenLog('Initializing device failed. ' + e);
+        }
+    });
+    deviceList.appendChild(deviceItem);
+}
+
+// Select target device and connect it
+function connectDevice(device) {
+    onScreenLog('Device selected: ' + device.name);
+
+    if (!device) {
+        onScreenLog('No devices found. You must request a device first.');
+    } else if (connectingUUIDSet.has(device.id) || connectedUUIDSet.has(device.id)) {
+        onScreenLog('Already connected to this device.');
+    } else {
+        connectingUUIDSet.add(device.id);
+        initializeCardForDevice(device);
+
+        // Wait until the requestDevice call finishes before setting up the disconnect listner
+        const disconnectCallback = () => {
+            updateConnectionStatus(device, 'disconnected');
+            device.removeEventListener('gattserverdisconnected', disconnectCallback);
+        };
+        device.addEventListener('gattserverdisconnected', disconnectCallback);
+
+        onScreenLog('Connecting ' + device.name);
+        device.gatt.connect().then(() => {
+            updateConnectionStatus(device, 'connected');
+            connectingUUIDSet.delete(device.id);
+
+            refreshValues(device).catch(e => onScreenLog(`ERROR on refreshValues(): ${e}\n${e.stack}`));
+        }).catch(e => {
+            flashSDKError(e);
+            onScreenLog(`ERROR on gatt.connect(${device.id}): ${e}`);
+            updateConnectionStatus(device, 'error');
+            connectingUUIDSet.delete(device.id);
+        });
+    }
+}
+
+// Setup device information card
+function initializeCardForDevice(device) {
+    const template = document.getElementById('device-template').cloneNode(true);
+    const cardId = 'device-' + device.id;
+
+    template.style.display = 'block';
+    template.setAttribute('id', cardId);
+    template.querySelector('.card > .card-header > .device-name').innerText = device.name;
+
+    // Device disconnect button
+    template.querySelector('.device-disconnect').addEventListener('click', () => {
+        onScreenLog('Clicked disconnect button');
+        device.gatt.disconnect();
+    });
+
+    template.querySelector('.refresh-value').addEventListener('click', () => {
+        refreshValues(device).catch(e => onScreenLog(`ERROR on refreshValues(): ${e}\n${e.stack}`));
+    });
+
+    // Remove existing same id card
+    const oldCardElement = getDeviceCard(device);
+    if (oldCardElement && oldCardElement.parentNode) {
+        oldCardElement.parentNode.removeChild(oldCardElement);
+    }
+
+    document.getElementById('device-cards').appendChild(template);
+    onScreenLog('Device card initialized: ' + device.name);
+}
+
+// Update Connection Status
+function updateConnectionStatus(device, status) {
+    if (status == 'connected') {
+        onScreenLog('Connected to ' + device.name);
+        connectedUUIDSet.add(device.id);
+
+        const statusBtn = getDeviceStatusButton(device);
+        statusBtn.setAttribute('class', 'device-status btn btn-outline-primary btn-sm disabled');
+        statusBtn.innerText = "Connected";
+        getDeviceDisconnectButton(device).style.display = 'inline-block';
+        getDeviceCardBody(device).style.display = 'block';
+    } else if (status == 'disconnected') {
+        onScreenLog('Disconnected from ' + device.name);
+        connectedUUIDSet.delete(device.id);
+
+        const statusBtn = getDeviceStatusButton(device);
+        statusBtn.setAttribute('class', 'device-status btn btn-outline-secondary btn-sm disabled');
+        statusBtn.innerText = "Disconnected";
+        getDeviceDisconnectButton(device).style.display = 'none';
+        getDeviceCardBody(device).style.display = 'none';
+        document.getElementById(device.id).classList.remove('active');
+    } else {
+        onScreenLog('Connection Status Unknown ' + status);
+        connectedUUIDSet.delete(device.id);
+
+        const statusBtn = getDeviceStatusButton(device);
+        statusBtn.setAttribute('class', 'device-status btn btn-outline-danger btn-sm disabled');
+        statusBtn.innerText = "Error";
+        getDeviceDisconnectButton(device).style.display = 'none';
+        getDeviceCardBody(device).style.display = 'none';
+        document.getElementById(device.id).classList.remove('active');
+    }
+}
+
+async function refreshValues(device) {
+    const pressureCharacteristic = await getCharacteristic(
+        device, ENVIRONMENTAL_SENSING_SERVICE_UUID, PRESSURE_CHARACTERISTIC_UUID);
+    const temperatureCharacteristic = await getCharacteristic(
+        device, ENVIRONMENTAL_SENSING_SERVICE_UUID, TEMPERATURE_CHARACTERISTIC_UUID);
+    const humidityCharacteristic = await getCharacteristic(
+        device, ENVIRONMENTAL_SENSING_SERVICE_UUID, HUMIDTY_CHARACTERISTIC_UUID);
+    const co2Characteristic = await getCharacteristic(
+        device, AIR_QUALITY_MONITOR_SERVICE_UUID, CO2_CHARACTERISTIC_UUID);
+    const tvocCharacteristic = await getCharacteristic(
+        device, AIR_QUALITY_MONITOR_SERVICE_UUID, TVOC_CHARACTERISTIC_UUID);
+
+    const pressureBuffer = await readCharacteristic(pressureCharacteristic).catch(e => {
+        return null;
+    });
+    const temperatureBuffer = await readCharacteristic(temperatureCharacteristic).catch(e => {
+        return null;
+    });
+    const humidityBuffer = await readCharacteristic(humidityCharacteristic).catch(e => {
+        return null;
+    });
+    const co2Buffer = await readCharacteristic(co2Characteristic).catch(e => {
+        return null;
+    });
+    const tvocBuffer = await readCharacteristic(tvocCharacteristic).catch(e => {
+        return null;
+    });
+
+    if (pressureBuffer !== null) {
+        const pressureValue = pressureBuffer.getUint32(0, true) / 10.0 / 100.0;
+        getDevicePressureField(device).innerText = `${pressureValue.toFixed(0)} hPa`;
+    }
+    if (temperatureBuffer !== null) {
+        const temperatureValue = temperatureBuffer.getInt16(0, true) / 100.0;
+        getDeviceTemperatureField(device).innerText = `${temperatureValue.toFixed(1)} ℃`;
+    }
+    if (humidityBuffer !== null) {
+        const humidityValue = humidityBuffer.getUint16(0, true) / 100.0;
+        getDeviceHumidityField(device).innerText = `${humidityValue.toFixed(1)} %`;
+    }
+    if (co2Buffer !== null) {
+        const co2Value = co2Buffer.getUint16(0, true);
+        getDeviceCo2Field(device).innerText = `${co2Value} ppm`;
+    }
+    if (tvocBuffer !== null) {
+        const tvocValue = tvocBuffer.getUint16(0, true);
+        getDeviceTvocField(device).innerText = `${tvocValue} ppb`;
+    }
+}
+
+async function readCharacteristic(characteristic) {
+    const response = await characteristic.readValue().catch(e => {
+        onScreenLog(`Error reading ${characteristic.uuid}: ${e}`);
+        throw e;
+    });
+    if (response) {
+        onScreenLog(`Read ${characteristic.uuid}: ${buf2hex(response.buffer)}`);
+        const values = new DataView(response.buffer);
+        return values;
+    } else {
+        throw 'Read value is empty?';
+    }
+}
+
+async function writeCharacteristic(characteristic, command) {
+    await characteristic.writeValue(new Uint8Array(command)).catch(e => {
+        onScreenLog(`Error writing ${characteristic.uuid}: ${e}`);
+        throw e;
+    });
+    //onScreenLog(`Wrote ${characteristic.uuid}: ${command}`);
+}
+
+async function getCharacteristic(device, serviceId, characteristicId) {
+    const service = await device.gatt.getPrimaryService(serviceId).catch(e => {
+        flashSDKError(e);
+        throw e;
+    });
+    const characteristic = await service.getCharacteristic(characteristicId).catch(e => {
+        flashSDKError(e);
+        throw e;
+    });
+    onScreenLog(`Got characteristic ${serviceId} ${characteristicId} ${device.id}`);
+    return characteristic;
+}
+
+function getDeviceCard(device) {
+    return document.getElementById('device-' + device.id);
+}
+
+function getDeviceCardBody(device) {
+    return getDeviceCard(device).getElementsByClassName('card-body')[0];
+}
+
+function getDeviceStatusButton(device) {
+    return getDeviceCard(device).getElementsByClassName('device-status')[0];
+}
+
+function getDeviceDisconnectButton(device) {
+    return getDeviceCard(device).getElementsByClassName('device-disconnect')[0];
+}
+
+function getDeviceTemperatureField(device) {
+    return getDeviceCard(device).getElementsByClassName('temperature-value')[0];
+}
+
+function getDeviceHumidityField(device) {
+    return getDeviceCard(device).getElementsByClassName('humidity-value')[0];
+}
+
+function getDevicePressureField(device) {
+    return getDeviceCard(device).getElementsByClassName('pressure-value')[0];
+}
+
+function getDeviceCo2Field(device) {
+    return getDeviceCard(device).getElementsByClassName('co2-value')[0];
+}
+
+function getDeviceTvocField(device) {
+    return getDeviceCard(device).getElementsByClassName('tvoc-value')[0];
+}
+
+function renderVersionField() {
+    const element = document.getElementById('sdkversionfield');
+    const versionElement = document.createElement('p')
+        .appendChild(document.createTextNode('SDK Ver: ' + liff._revision));
+    element.appendChild(versionElement);
+}
+
+function flashSDKError(error){
+    window.alert('SDK Error: ' + error.code);
+    window.alert('Message: ' + error.message);
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function buf2hex(buffer) { // buffer is an ArrayBuffer
+    return Array.prototype.map.call(new Uint8Array(buffer), x => ('00' + x.toString(16)).slice(-2)).join('');
+}
